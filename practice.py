ba98ef659a6a6b10ee92b90daa293bda29b75a8b
Siddharoodha <siddharoodha10@gmail.com>
2021-10-20 16:11:48 +0530

    third commit

--- practice.py
+++ practice.py
@@ -9,11 +9,14 @@ def div(x,y):
     return x//y
 def sub(x,y):
     return x-y
+def rem(x,y):
+    return x%y
     
-with open("output.txt",'a') as f:
+with open("output.txt",'w') as f:
     x=30;y=10
     f.write("addationof two number is: "+str(add(x,y))+"\n")
     f.write("substraction of two numbers is:"+str(sub(x,y))+"\n")
     f.write("Multiplication of two number is:"+str(mul(x,y))+'\n')
     f.write("division of the two numbers is:"+str(div(x,y))+'\n')
+    f.write("Remendr of the x and y is:"+str(rem(x,y))+"\n")
     f.write('\n')
